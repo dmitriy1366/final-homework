ed894f137658db4369d82a93d399fa34ed4eaa5a
Дмитрий <119155029+dmitriy1366@users.noreply.github.com>
2023-02-22 14:10:21 +0300

    условие первой задачи

--- hw_10_task.1.py
+++ hw_10_task.1.py
@@ -6,15 +6,4 @@
 """
 
 
-def word_format(lst):
-    for elem in lst:
-        print(f"{type(elem)}\n{elem}")
 
-
-words = ['разработка', 'сокет', 'декоратор']
-words_upoint = ['\u0440\u0430\u0437\u0440\u0430\u0431\u043e\u0442\u043a\u0430',
-                '\u0441\u043e\u043a\u0435\u0442',
-                '\u0434\u0435\u043a\u043e\u0440\u0430\u0442\u043e\u0440']
-
-word_format(words)
-word_format(words_upoint)
